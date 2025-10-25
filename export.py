76a0ba1e8bf16e9e38a5f123d8546684bbd1f878
Frédéric Tronel <frederic.tronel@centralesupelec.fr>
2025-10-25 14:54:44 +0200

    Une première version basique avec un début de parsing des options et une documentation rudimentaire

--- export.py
+++ export.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
+"""Module providing a way to transcode a set of music files into standardize MP3 files."""
+
+import argparse
+import logging
+import coloredlogs
+
+def main():
+    """Main function of the program."""
+    logger = logging.getLogger(__name__)
+    coloredlogs.install()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v","--verbose", action='store_true', dest='verbose', help="Debug.")
+
+    args = parser.parse_args()
+    logger.info('Arguments: %s',args)
+
+    if args.verbose:
+        logger.info('Setting logging to debug mode')
+        coloredlogs.set_level(level=logging.DEBUG)
+
+    logger.debug('Arguments: %s',args)
 
 
-def main:
-    pass
-
-
+if __name__ == "__main__":
+    main()
