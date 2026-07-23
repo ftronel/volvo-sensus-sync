c061ee9ef28e7dd447aedb8df0eef2083fcc84e2
Frédéric Tronel <frederic.tronel@centralesupelec.fr>
2026-07-23 15:22:29 +0200

    Typo.

--- export.py
+++ export.py
@@ -80,7 +80,7 @@ def main():
         audios[inode] = { 'artist': artist, 'album': album, 'title': title, 'track': track}
 
     for f in audios:
-        print(files[f])
+        print(audio[f])
 
 
     
