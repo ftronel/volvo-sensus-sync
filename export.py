f317e79655768ccd1bbd48463e97f05f4cc41e04
Frédéric Tronel <frederic.tronel@centralesupelec.fr>
2026-07-24 16:45:47 +0200

    Typo.

--- export.py
+++ export.py
@@ -136,7 +136,7 @@ def main():
                 tracks = discs[disc]
                 for track in tracks:
                     logger.debug("Considering title: %s", track['title'])
-                    final_path = f"{dest_path}/{track['track']:02d}{track['title']}.mp3"
+                    final_path = f"{dest_path}/{track['track']:02d}-{track['title']}.mp3"
                     logger.debug("Testing if file %s exists", final_path)
                     dest = Path(final_path)
                     if dest.exists():
