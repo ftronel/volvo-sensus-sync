b9d5a609137e7627814cc0e3599a03b66cab2535
Frédéric Tronel <frederic.tronel@centralesupelec.fr>
2026-07-24 22:24:43 +0200

    Improve type checking.

--- export.py
+++ export.py
@@ -88,7 +88,7 @@ def get_metadata(files: list[Path]) -> dict[str,dict[str,dict[int,list[dict[str,
     return res
 
 @typechecked
-def determine_conversions(audios: dict[str,dict[str,dict[int,list[dict]]]], export_dir: str) -> list:
+def determine_conversions(audios: dict[str,dict[str,dict[int,list[dict[str, int|str|Path]]]]], export_dir: str) -> list:
     logger = logging.getLogger(__name__)
     res = []
 
