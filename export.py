36d6bb0723cb0a837452e7ed54c2fee13120137e
Frédéric Tronel <frederic.tronel@centralesupelec.fr>
2026-08-01 08:42:26 +0200

    Fix bug.

--- export.py
+++ export.py
@@ -268,8 +268,8 @@ def determine_conversions(audios: dict[str,dict[str,dict[int,list[Track]]]],
                     dest_path = path2
                 tracks = discs[disc]
                 for track in tracks:
-                    logger.debug("Considering title: %s", track['title'])
-                    final_path = f"{dest_path}/{track['track']:02d}-{track['title']}.mp3"
+                    logger.debug("Considering title: %s", track.title)
+                    final_path = f"{dest_path}/{track['track']:02d}-{track.title}.mp3"
                     logger.debug("Testing if file %s exists", final_path)
                     dest = Path(final_path)
                     if dest.exists():
